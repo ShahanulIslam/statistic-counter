7b81249fc00f59fd79b6ed5bee5b1997ea221ec3
Your Name <shahanricky86@gmail.com>
2023-11-21 01:38:41 +0600

    update font size

--- src/components/ReactCounter/ReactCounter.jsx
+++ src/components/ReactCounter/ReactCounter.jsx
@@ -14,31 +14,31 @@ const ReactCounter = () => {
             <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-5 m-7 bg-transparent">
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <AiOutlineClockCircle className="text-[#20e494] text-5xl mx-auto"></AiOutlineClockCircle> */}
-                    <h1 className="text-5xl font-bold text-center">
+                    <h1 className="text-4xl font-bold text-center">
                         {counterOn && <CountUp start={0} end={225} duration={2} delay={0}></CountUp>}
                     </h1>
-                    <h4 className="text-xl text-center">Housing units sold</h4>
+                    <h4 className="text-center">Housing units sold</h4>
                 </div>
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <CgWebsite className="text-[#20e494] text-5xl mx-auto"></CgWebsite> */}
-                    <h1 className="text-5xl font-bold text-center">
+                    <h1 className="text-4xl font-bold text-center">
                         {counterOn && <CountUp start={0} end={45} duration={2} delay={0}></CountUp>}
                     </h1>
-                    <h4 className="text-xl text-center">Active Projects</h4>
+                    <h4 className="text-center">Active Projects</h4>
                 </div>
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <FaAward className="text-[#20e494] text-5xl mx-auto"></FaAward> */}
-                    <h1 className="text-5xl font-bold text-center">
+                    <h1 className="text-4xl font-bold text-center">
                         {counterOn && <CountUp start={0} end={98} suffix="%" duration={2} delay={0}></CountUp>}
                     </h1>
-                    <h4 className="text-xl text-center">Client Satisfaction</h4>
+                    <h4 className="text-center">Client Satisfaction</h4>
                 </div>
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <BsFillPersonCheckFill className="text-[#20e494] text-5xl mx-auto"></BsFillPersonCheckFill> */}
-                    <h1 className="text-5xl font-bold text-center">
+                    <h1 className="text-4xl font-bold text-center">
                         {counterOn && <CountUp start={0} prefix="$" suffix=".75M" end={33} duration={8} delay={0}></CountUp>}
                     </h1>
-                    <h4 className="text-xl text-center">Created in Revenue</h4>
+                    <h4 className="text-center">Created in Revenue</h4>
                 </div>
             </div>
         </ScrollTrigger>
