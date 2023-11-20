a851cdc831a85f2c22d674b30476c4573ea7e4f8
Your Name <shahanricky86@gmail.com>
2023-11-21 01:24:23 +0600

    update

--- src/components/ReactCounter/ReactCounter.jsx
+++ src/components/ReactCounter/ReactCounter.jsx
@@ -2,10 +2,10 @@
 import ScrollTrigger from "react-scroll-trigger";
 import CountUp from "react-countup";
 import { useState } from "react";
-import { AiOutlineClockCircle } from 'react-icons/ai';
-import { CgWebsite } from 'react-icons/cg';
-import { BsFillPersonCheckFill } from 'react-icons/bs';
-import { FaAward } from 'react-icons/fa';
+// import { AiOutlineClockCircle } from 'react-icons/ai';
+// import { CgWebsite } from 'react-icons/cg';
+// import { BsFillPersonCheckFill } from 'react-icons/bs';
+// import { FaAward } from 'react-icons/fa';
 
 const ReactCounter = () => {
     const [counterOn, setCounterOn] = useState(false);
@@ -15,7 +15,7 @@ const ReactCounter = () => {
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <AiOutlineClockCircle className="text-[#20e494] text-5xl mx-auto"></AiOutlineClockCircle> */}
                     <h1 className="text-5xl font-bold text-center">
-                        {counterOn && <CountUp start={0} end={15} duration={2} delay={0}></CountUp>}
+                        {counterOn && <CountUp start={0} end={225} duration={2} delay={0}></CountUp>}
                     </h1>
                     <h4 className="text-xl text-center">Housing units sold</h4>
                 </div>
@@ -24,24 +24,22 @@ const ReactCounter = () => {
                     <h1 className="text-5xl font-bold text-center">
                         {counterOn && <CountUp start={0} end={45} duration={2} delay={0}></CountUp>}
                     </h1>
-                    <h4 className="text-xl text-center">New Projects</h4>
-                </div>
-                <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
-                    {/* <BsFillPersonCheckFill className="text-[#20e494] text-5xl mx-auto"></BsFillPersonCheckFill> */}
-                    <h1 className="text-5xl font-bold text-center">
-                        {counterOn && <CountUp start={0} end={350} duration={2} suffix="+" delay={0}></CountUp>}
-                    </h1>
-                    <h4 className="text-xl text-center">Successful Project</h4>
+                    <h4 className="text-xl text-center">Active Projects</h4>
                 </div>
                 <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
                     {/* <FaAward className="text-[#20e494] text-5xl mx-auto"></FaAward> */}
                     <h1 className="text-5xl font-bold text-center">
-                        {counterOn && <CountUp start={0} end={93} suffix="%" duration={2} delay={0}></CountUp>}
+                        {counterOn && <CountUp start={0} end={98} suffix="%" duration={2} delay={0}></CountUp>}
                     </h1>
                     <h4 className="text-xl text-center">Client Satisfaction</h4>
                 </div>
-
-
+                <div className="bg-white text-black p-8 rounded-2xl space-y-3 transform transition hover:scale-105">
+                    {/* <BsFillPersonCheckFill className="text-[#20e494] text-5xl mx-auto"></BsFillPersonCheckFill> */}
+                    <h1 className="text-5xl font-bold text-center">
+                        {counterOn && <CountUp start={0} prefix="$" suffix=".75 M" end={33} duration={8} delay={0}></CountUp>}
+                    </h1>
+                    <h4 className="text-xl text-center">Created in Revenue</h4>
+                </div>
             </div>
         </ScrollTrigger>
     );
